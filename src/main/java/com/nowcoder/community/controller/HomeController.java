8f9f7a8fa3fab8d973a6ae11a2b1b70671045aa3
YangJin <1160684665@qq.com>
2020-08-20 21:11:31 +0800

    v 1.0.1

--- src/main/java/com/nowcoder/community/controller/HomeController.java
+++ src/main/java/com/nowcoder/community/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.nowcoder.community.controller;
 
 import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.Page;
 import com.nowcoder.community.entity.User;
 import com.nowcoder.community.service.DiscussPostService;
 import com.nowcoder.community.service.UserService;
@@ -8,6 +9,7 @@ import org.checkerframework.checker.units.qual.A;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -25,9 +27,16 @@ public class HomeController {
     @Autowired
     UserService userService;
 
-    @RequestMapping(path = "/index",method = RequestMethod.GET)
-    public String getIndexPage(Model model){
-        List<DiscussPost> list = discussPostService.findDiscussPosts(0,0,10);
+    @GetMapping(path = "/index")
+    public String getIndexPage(Model model, Page page){
+        // 方法调用前,SpringMVC会自动实例化Model和Page,并将Page注入Model.
+        // 所以,在thymeleaf中可以直接访问Page对象中的数据.
+        // limit 和 current 由URI传递，否则使用默认值
+        page.setRows(discussPostService.findDiscussPostRows(0));
+        page.setPath("/index");
+
+    List<DiscussPost> list =
+        discussPostService.findDiscussPosts(0, page.getOffset(), page.getLimit());
         List<Map<String,Object>> discussPosts = new ArrayList<>();
         if (list != null){
             for (DiscussPost post:list){
@@ -39,7 +48,7 @@ public class HomeController {
             }
         }
         model.addAttribute("discussPosts",discussPosts);
-        return "index";
+        return "/index";
     }
 
 }
