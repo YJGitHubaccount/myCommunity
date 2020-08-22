b1d5f70fe06af4d76e378ba3e22bd1aed94ab30c
YangJin <1160684665@qq.com>
2020-08-22 11:19:34 +0800

    增加账号激活功能

--- README.md
+++ README.md
@@ -5,3 +5,19 @@
 ### day 2020/8/21
 修改.gitignore没起效
 git rm -r --cached .    //删除项目缓存即可
+
+### day /2020/8/22
+```
+<div class="col-sm-4">
+    <img th:src="@{/kaptcha}" id="kaptcha" style="width:100px;height:40px;" class="mr-2"/>
+    <a href="javascript:refresh_kaptcha();" class="font-size-12 align-bottom">刷新验证码</a>
+</div>
+```
+修改为
+```
+<div class="col-sm-4">
+    <img th:src="@{/img/captcha.png}" id="kaptcha" style="width:100px;height:40px;" class="mr-2"/>
+    <a href="javascript:refresh_kaptcha();" class="font-size-12 align-bottom">刷新验证码</a>
+</div>
+```
+
--- src/main/java/com/nowcoder/community/controller/LoginController.java
+++ src/main/java/com/nowcoder/community/controller/LoginController.java
@@ -1,26 +1,50 @@
 package com.nowcoder.community.controller;
 
+import com.google.code.kaptcha.Producer;
 import com.nowcoder.community.entity.User;
 import com.nowcoder.community.service.UserService;
+import com.nowcoder.community.util.CommunityConstant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.mail.Session;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Map;
 
 @Controller
-public class LoginController {
+public class LoginController implements CommunityConstant {
 
     @Autowired
-    UserService userService;
+    private UserService userService;
+
+    @Autowired
+    private Producer kaptchaProducer;
 
     @GetMapping(path = "/register")
     public String getRegisterPage(){
         return "/site/register";
     }
 
+    @GetMapping(path = "/login")
+    public String getLoginPage(){
+        return "/site/login";
+    }
+
+    /**
+     * 提交注册表单，自动对应生成user
+     * @param model
+     * @param user
+     * @return
+     */
     @PostMapping(path = "/register")
     public String registerUser(Model model, User user){
         Map<String,Object> map = userService.register(user);
@@ -35,6 +59,53 @@ public class LoginController {
             model.addAttribute("passwordMsg",map.get("passwordMsg"));
             model.addAttribute("emailMsg",map.get("emailMsg"));
             return "/site/register";
+        }
+    }
+
+    /**
+     * 激活账号
+     * @param model
+     * @param userId
+     * @param code
+     * @return
+     */
+    // http://localhost:8080/community/activation/101/code
+    @GetMapping(path = "/activation/{userId}/{code}")
+    public String activation(Model model, @PathVariable("userId") int userId,@PathVariable("code") String code){
+        int result = userService.activation(userId,code);
+        if (result == ACTIVATION_SUCCESS){
+            model.addAttribute("msg","激活成功，您的账号已经可以正常使用了!");
+            model.addAttribute("target","/login");//跳转页面
+        }else if(result == ACTIVATION_REPEAT){
+            model.addAttribute("msg","无效操作，该账号已经激活过了!");
+            model.addAttribute("target","/index");
+        }else {
+            model.addAttribute("msg","激活失败!");
+            model.addAttribute("target","/index");
+        }
+        return "/site/operate-result";
+    }
+
+    /**
+     * 浏览器访问此路径申请验证码
+     * @param response
+     * @param session
+     */
+    @GetMapping("/kaptcha")
+    public void getKaptcha(HttpServletResponse response, HttpSession session){
+        //生成验证码
+        String text = kaptchaProducer.createText();
+        BufferedImage image = kaptchaProducer.createImage(text);
+
+        //将验证码存入session
+        session.setAttribute("kaptcha",text);
+
+        //将图片输出给浏览器
+        response.setContentType("image/png");
+        try {
+            OutputStream outputStream = response.getOutputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
